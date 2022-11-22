e2389e30655200e44014a8a3def0cc80fbd42704
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-22 15:27:38 -0600

    added header styles and content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,48 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href='/'>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
+          <DiCssdeck size='3rem' /> <Span>theGlamTechie Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href='#projects'>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#tech'>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href='#projects'>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com/shay90210'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/shannontatibentley/'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://twitter.com/ShannonTatianna'>
+        <AiFillTwitterCircle size='3rem' />
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
