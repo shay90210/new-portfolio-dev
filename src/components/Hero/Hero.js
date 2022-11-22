f380078c964f0bcbeb49e6e90ee4fa826ea1e1c4
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-22 15:47:12 -0600

    updated hero and uncommented animation in index.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = () => (
         your friendly <i>web developer</i>
       </SectionTitle>
       <SectionText>
-        The purpose of theGlamTechie is to present my learned skills of JavaScript and React to employers and to educate newbies like me on how to get into tech by spreading my joys of learning technology. 
+        The purpose of theGlamTechie is to present my learned skills of JavaScript and React to employers and to educate newbies like me on how to get into tech by spreading my joys of learning technology and building functional apps.
       </SectionText>
       <Button onClick={() => window.location = '/'}>Learn More</Button>
     </LeftSection>
