c66205cee835fbef7c576b125b88673e5117eff3
Shannon Bentley <bentley.shannon@yahoo.com>
2022-11-27 17:11:26 -0600

    updated code spacing

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,8 +2,8 @@ export const projects = [
   {
     title: "The Teachers' Club Bootstrap Site",
     description: "This project is my first deployed website called The Teachers' Club. It is a simple e-commerce website that allows users to search for and purchase teacher apparel items of their choice.",
-      image: '/images/teachersclub1.png',
-      tags: ['JavaScript', 'Bootstrap', 'SASS'],
+    image: '/images/teachersclub1.png',
+    tags: ['JavaScript', 'Bootstrap', 'SASS'],
     source: 'https://shay90210.github.io/teachers-club/',
     visit: 'https://github.com/shay90210/teachers-club',
     id: 0,
@@ -11,9 +11,5 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2022, text: 'Started my tech journey', },
 ];
