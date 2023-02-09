a759e64e7a4a4003324b1e3245952e38741f7931
Shannon Bentley <bentley.shannon@yahoo.com>
2023-02-09 12:25:08 -0600

    updated projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const projects = [
-  {
+  /*{
     title: "E-commerce React Site",
     description: "This project is a redention of 'The Teachers' Club' Bootstrap project; however, it is coded in React.js. This project is a work in progress that will include all JavaScript functions from the original project coded as slices and components, databases items selected, and HTTP protocols when sending requests to fetch item data.",
     image: '/images/reactproject.png',
@@ -16,15 +16,15 @@ export const projects = [
     source: '/',
     visit: 'https://github.com/shay90210/talkfem-app-react',
     id: 1,
-  },
+  },*/
   {
     title: "E-Commerce Bootstrap Site",
-    description: "This project is my first deployed website called The Teachers' Club. It is an e-commerce website that allows users to search for and purchase teacher apparel items. The project is primarily coded using JavaScript functions to increment/decrement items, remove items, and hold items in a basket when the page refreshes.",
+    description: "This project is my first deployed website called The Teachers' Club. It is an e-commerce website that sells teacher apparel. The project is primarily coded using JavaScript functions to increment/decrement items, remove items, and hold items in a basket when the page refreshes. While I have only been coding for 9 months before deploying this site, I still believed that it had potential for improvement such making the website more responsive to other designs, adding an authentication for login users/repeat customers, and adding a payment feature. This project will be revamped using JavaScript and React.",
     image: '/images/teachersclub1.png',
     tags: ['JavaScript', 'Bootstrap', 'SASS'],
     source: 'https://shay90210.github.io/teachers-club/',
     visit: 'https://github.com/shay90210/teachers-club',
-    id: 2,
+    id: 1,
   }
 ];
 
