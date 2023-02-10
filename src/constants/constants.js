4260b4d1faf5effa17d50247e6ce6926da733f9d
Shannon Bentley <bentley.shannon@yahoo.com>
2023-02-10 09:26:02 -0600

    updated timeline

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -19,7 +19,7 @@ export const projects = [
   },*/
   {
     title: "E-Commerce Bootstrap Site",
-    description: "This project is my first deployed website called The Teachers' Club. It is an e-commerce website that sells teacher apparel. The project is primarily coded using JavaScript functions to increment/decrement items, remove items, and hold items in a basket when the page refreshes. <br/> While I have only been coding for 9 months before deploying this site, I still believed that it had potential for improvement such making the website more responsive to other designs, adding an authentication for login users/repeat customers, and adding a payment feature. This project will be revamped using JavaScript and React.",
+    description: "This project is my first deployed website called The Teachers' Club. It is an e-commerce website that sells teacher apparel. The project is primarily coded using JavaScript functions to increment/decrement items, remove items, and hold items in a basket when the page refreshes. While I have only been coding for 9 months before deploying this site, I still believed that it had potential for improvement such making the website more responsive to other designs, adding an authentication for login users/repeat customers, and adding a payment feature. This project will be revamped using JavaScript and React.",
     image: '/images/teachersclub1.png',
     tags: ['JavaScript', 'Bootstrap', 'SASS'],
     source: 'https://shay90210.github.io/teachers-club/',
