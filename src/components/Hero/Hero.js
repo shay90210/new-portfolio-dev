034bd899a8a3f361404410ef9b88639d0333e61b
Shannon Bentley <bentley.shannon@yahoo.com>
2023-02-25 19:12:12 -0600

    update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,6 +11,14 @@ const Hero = () => (
         Hi! I'm Shannon Bentley, <br /> 
         your friendly <i>software engineer</i>
       </SectionTitle>
+      <SectionText>
+          I created theGlamTechie to showcase my proficiency in programming languages and
+          to inspire other beginners to pursue a career in tech. Through my portfolio, I
+          hope to impress future employers with my skills and dedication to software
+          engineering. Each project is a testament to my ability to learn and apply new
+          technologies. I am constantly seeking new opportunities to expand my knowledge
+          and contribute to the tech community.
+      </SectionText>
       <Button onClick={() => window.location = 'https://docs.google.com/document/d/1zoYjCT-DwCfqZ6cZH2STGhL-uGPWHAlBu6rrS4LBBWA/edit'}>Resume</Button>
     </LeftSection>
   </Section>
