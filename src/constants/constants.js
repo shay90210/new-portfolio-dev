510024e9e2a492cc1adda6eba568133fa29a9bd1
Shannon Bentley <bentley.shannon@yahoo.com>
2023-03-08 15:46:50 -0600

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,13 +1,5 @@
 export const projects = [
-  /*{
-    title: "E-commerce React Site",
-    description: "This project is a redention of 'The Teachers' Club' Bootstrap project; however, it is coded in React.js. This project is a work in progress that will include all JavaScript functions from the original project coded as slices and components, databases items selected, and HTTP protocols when sending requests to fetch item data.",
-    image: '/images/reactproject.png',
-    tags: ['React.js', 'SASS'],
-    source: '/',
-    visit: 'https://github.com/shay90210/teachers-app-site',
-    id: 0,
-  },
+  /*
   {
     title: "Podcast React Site",
     description: "This project is an introduction into a mobile version that I will create later. For now, this project will incorporate databases to hold already created episodes from anchor.com and HTTP protocols to retrieve the episode data. This project is currently still a work in progress.",
@@ -18,20 +10,30 @@ export const projects = [
     id: 1,
   },*/
   {
+    title: "React && Typescript E-Commerce Application",
+    description: "I completed my React project using Typescript and Redux. In my last e-commerce project, I went through significant errors that were unnoticeable. I couldn't define where the errors came from after completing an entire project, yet my project would not deploy on Netlify. To rectify the intricacies that I faced and ensure a successful project, I redid this project using only React and Typescript to ensure that each step that I code improves my editor support and catches mistakes in my editor before any runtime for testing. This project features adding items to cart, incrementing/decrementing items, removing items and a cart summary on an offcanvas component that shows the price, total, and # of items in the cart. ",
+    image: '/images/screencapture-localhost-3000-about-2023-03-08-14_53_05.png',
+    tags: ['React', 'TypeScript'],
+    source: 'https://ephemeral-pothos-06df43.netlify.app/',
+    visit: 'https://github.com/shay90210/teachers-club-app',
+    id: 1,
+  },
+  {
     title: "E-Commerce Bootstrap Site",
     description: "I created The Teachers' Club, an e-commerce website that sells teacher apparel, as my first deployed website. The website uses JavaScript functions to increment/decrement items, remove items, and hold items in a basket when the page refreshes. While I recognize that there is potential for improvement, such as adding an authentication for login users/repeat customers and a payment feature, I am proud of what I accomplished with just 9 months of coding experience. I am currently revamping the project using JavaScript and React to make the website more responsive and efficient.",
     image: '/images/teachersclub1.png',
     tags: ['JavaScript'],
     source: 'https://shay90210.github.io/teachers-club/',
     visit: 'https://github.com/shay90210/teachers-club',
-    id: 1,
+    id: 0,
   }
 ];
 
 export const TimeLineData = [
-  { year: 2015, text: 'May - Graduated from Marquette University with a Bachelor of Science Degree in Education'},
+  { year: 2015, text: 'May - Graduated from Marquette University with a Bachelor of Science Degree in Education' },
   { year: 2022, text: 'January - Started my tech journey', },
-  { year: 2022, text: 'June - Completed Nucamp Web Development Fundamentals Program'},
-  { year: 2022, text: 'September - Deployed my first website'},
-  { year: 2022, text: 'November - Completed Nucamp Front-End & Mobile Development Program',},
+  { year: 2022, text: 'June - Completed Nucamp Web Development Fundamentals Program' },
+  { year: 2022, text: 'September - Deployed my first website coded in HTML, CSS, & JavaScript' },
+  { year: 2022, text: 'November - Completed Nucamp Front-End & Mobile Development Program', },
+  { year: 2023, text: 'March - Deployed my first application coded in React and TypeScript' }
 ];
