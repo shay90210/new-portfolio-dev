444dc50a9e84a2caab3da780433bbd339bc4fed3
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-05 04:44:50 +0000

    updated footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>Powered by theGlamTechie &copy; 2023</Slogan>
+          <Slogan>Powered by theGlamTechie &copy; 2024</Slogan>
         </CompanyContainer>
         {/*<SocialContainer>
             <SocialIcons href='https://github.com/shay90210'>
