7c05548cd782fb7ab0b847cd8942e4812a6ab7f5
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-09 03:15:42 +0000

    update

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -49,7 +49,10 @@ const Timeline = () => {
           As a Support Tech passionate about technology, I continuously
           learn about the industry's intricacies. My boot camp experience and self-motivation
           have enabled me to acquire the necessary skills in coding languages
-          to succeed in my tech career. I am interested in combining software
+          to succeed in my tech career. 
+        </p>
+        <p>
+          I am interested in combining software
           engineering with cybersecurity, specifically in the areas of
           application security and DevOps. My goal is to develop innovative and secure
           applications that mitigate potential cyber threats, benefiting both users and
